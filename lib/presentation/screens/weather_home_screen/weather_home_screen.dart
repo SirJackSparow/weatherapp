6a4207de18bec5795dcddb7bd9764ee86dcc48e8
SirJackSparow <maulanabima785@gmail.com>
2024-05-14 18:17:10 -0700

    dev

--- lib/presentation/screens/weather_home_screen/weather_home_screen.dart
+++ lib/presentation/screens/weather_home_screen/weather_home_screen.dart
@@ -4,24 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:weatherapps/presentation/controllers/weather_home_controller/weather_home.dart';
-import 'package:weatherapps/const/utils/weather_app_string.dart';
+import 'package:weatherapps/const/utils/utils.dart';
 import 'package:weatherapps/data/entity/weather_entity.dart';
-import 'package:weatherapps/data/shared_pref/sharedprefs_service.dart';
 import 'package:weatherapps/presentation/controllers/conectivity/internate_connectivity_bloc.dart';
 import 'package:weatherapps/presentation/controllers/get_local_controller/get_local_data_bloc.dart';
-import 'package:weatherapps/presentation/controllers/settings_theme_controller/settings_theme_bloc.dart';
 import 'package:weatherapps/presentation/controllers/weather_home_controller/weather_home_controller_bloc.dart';
+import 'package:weatherapps/presentation/screens/weather_home_screen/widgets/drawer.dart';
 import 'package:weatherapps/presentation/screens/weather_home_screen/widgets/weather_offline_widget.dart';
 import 'package:weatherapps/presentation/screens/weather_home_screen/widgets/weather_online_widget.dart';
-
-import '../../../const/app_color.dart';
-import '../../../const/utils/weather_app_fonts.dart';
-import '../../../const/utils/weather_font_sizes.dart';
-import '../../../routes/weather_routes.dart';
-import '../../controllers/forecast_get_local_controller/get_forecast_local_bloc.dart';
-import '../../controllers/localdatabase_controller/save_data_local_bloc.dart';
-import '../../controllers/settings_farenheit_controller/setting_farenheit_bloc.dart';
 
 class WeatherHomeScreen extends StatefulWidget {
   const WeatherHomeScreen({super.key});
@@ -34,16 +24,12 @@ class _WeatherHomeState extends State<WeatherHomeScreen>
     with WidgetsBindingObserver, TickerProviderStateMixin {
   final GlobalKey<State> permissionDialogKey = GlobalKey<State>();
   bool isLocationServiceInitialized = false;
-  bool theme = false;
-  bool s = false;
 
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance.addObserver(this);
     initLocationService();
-    setTheme(context, null);
-    setFarenheit(context,null);
   }
 
   @override
@@ -56,57 +42,54 @@ class _WeatherHomeState extends State<WeatherHomeScreen>
   Widget build(BuildContext context) {
     return Scaffold(
         drawer: drawerWeather(),
-        body:
-            BlocBuilder<InternateConnectivityBloc, InternateConnectivityState>(
-          builder: (context, state) {
-            if (state is ConnectedState) {
-              return BlocConsumer<WeatherHomeControllerBloc,
-                  WeatherHomeControllerState>(
-                buildWhen: (previous, current) {
-                  return (previous is CurrentCityWeatherInfoLoading &&
-                          current is CurrentCityDataLoaded) ||
-                      (current is CurrentCityDataLoaded &&
-                          previous is CurrentCityDataLoaded);
-                },
-                listenWhen: (previous, current) {
-                  return previous != current;
-                },
-                builder: (context, state) {
-                  if (state is CurrentCityDataLoaded) {
-                    WeatherModel weatherCityModel = state.currentCityData;
-                    weatherCityModel.isCurrentCity = true;
-                    _saveCity(weatherCityModel, context);
-                    return WeatherUIWidget(
-                      weatherModel: weatherCityModel,
-                    );
-                  }
-                  return Container();
-                },
-                listener:
-                    (BuildContext context, WeatherHomeControllerState state) {
-                  if (state is CurrentCityWeatherInfoLoadingError) {
-                    log(state.errorMessage);
-                  }
-                },
-              );
-            } else if (state is NotConnectedState) {
-              _getLocalData(context);
-              _getForeCastData(context);
-              return BlocBuilder<GetLocalDataBloc, GetLocalDataState>(
-                builder: (context, states) {
-                  if (states is GetLocalDataLoaded) {
-                    return WeatherOfflineWidget(
-                        weatherModel: states.weatherModel);
-                  } else if (states is GetLocalDataWeatherError) {
-                    log(states.errorMessage);
-                  }
-                  return Container();
-                },
-              );
-            }
-            return Container();
-          },
-        ));
+        body: BlocBuilder<InternateConnectivityBloc, InternateConnectivityState>(
+      builder: (context, state) {
+        if (state is ConnectedState) {
+          return BlocConsumer<WeatherHomeControllerBloc,
+              WeatherHomeControllerState>(
+            buildWhen: (previous, current) {
+              return (previous is CurrentCityWeatherInfoLoading &&
+                      current is CurrentCityDataLoaded) ||
+                  (current is CurrentCityDataLoaded &&
+                      previous is CurrentCityDataLoaded);
+            },
+            listenWhen: (previous, current) {
+              return previous != current;
+            },
+            builder: (context, state) {
+              if (state is CurrentCityDataLoaded) {
+                WeatherModel weatherCityModel = state.currentCityData;
+                weatherCityModel.isCurrentCity = true;
+                HomeUtils.saveCity(weatherCityModel, context);
+                return WeatherUIWidget(
+                  weatherModel: weatherCityModel,
+                );
+              }
+              return Container();
+            },
+            listener: (BuildContext context, WeatherHomeControllerState state) {
+              if (state is CurrentCityWeatherInfoLoadingError) {
+                log(state.errorMessage);
+              }
+            },
+          );
+        } else if (state is NotConnectedState) {
+          HomeUtils.getLocalData(context);
+          HomeUtils.getForeCastData(context);
+          return BlocBuilder<GetLocalDataBloc, GetLocalDataState>(
+            builder: (context, states) {
+              if(states is GetLocalDataLoaded) {
+                return WeatherOfflineWidget(weatherModel: states.weatherModel);
+              } else if (states is GetLocalDataWeatherError) {
+                log(states.errorMessage);
+              }
+              return Container();
+            },
+          );
+        }
+        return Container();
+      },
+    ));
   }
 
   @override
@@ -128,7 +111,7 @@ class _WeatherHomeState extends State<WeatherHomeScreen>
   Future<void> initLocationService() async {
     if (!await Geolocator.isLocationServiceEnabled()) {
       if (mounted) {
-        await WeatherHome.showLocationServiceDialog(context, mounted);
+        await HomeUtils.showLocationServiceDialog(context, mounted);
       }
     } else {
       dismissPermissionDialog();
@@ -143,7 +126,7 @@ class _WeatherHomeState extends State<WeatherHomeScreen>
         permission = await Geolocator.requestPermission();
         if (permission == LocationPermission.denied) {
           if (mounted) {
-            WeatherHome.permissionDialog(
+            HomeUtils.permissionDialog(
                 context, permissionDialogKey, mounted, false, false);
           }
           return;
@@ -164,7 +147,7 @@ class _WeatherHomeState extends State<WeatherHomeScreen>
     }
     if (mounted) {
       isLocationServiceInitialized = true;
-      await WeatherHome.getPosition(
+      await HomeUtils.getPosition(
           context, mounted, permissionDialogKey, false, false);
     }
   }
@@ -174,118 +157,5 @@ class _WeatherHomeState extends State<WeatherHomeScreen>
         permissionDialogKey.currentContext != null) {
       Navigator.of(permissionDialogKey.currentContext!).pop();
     }
-  }
-
-  Widget drawerWeather() {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          DrawerHeader(
-            child: Text(
-              WeatherAppString.drawerHeader,
-              style: WeatherAppFonts.large(
-                      fontWeight: FontWeight.w700,
-                      color: WeatherAppColor.blackColor)
-                  .copyWith(fontSize: WeatherAppFontSize.s17),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(children: [
-              Text(
-                WeatherAppString.darkTheme,
-                style: WeatherAppFonts.medium(
-                        fontWeight: FontWeight.w400,
-                        color: WeatherAppColor.blackColor)
-                    .copyWith(fontSize: WeatherAppFontSize.s14),
-              ),
-              Expanded(child: Container()),
-              Switch(
-                  value: theme,
-                  activeColor: Colors.blue,
-                  onChanged: (bool value) {
-                    setState(() {
-                      theme = value;
-                      setTheme(context, theme);
-                    });
-                  })
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(children: [
-              Text(
-                WeatherAppString.farenheitName,
-                style: WeatherAppFonts.medium(
-                        fontWeight: FontWeight.w400,
-                        color: WeatherAppColor.blackColor)
-                    .copyWith(fontSize: WeatherAppFontSize.s14),
-              ),
-              Expanded(child: Container()),
-              Switch(
-                  value: s,
-                  activeColor: Colors.blue,
-                  onChanged: (bool value) {
-                    setState(() {
-                      s = value;
-                      setFarenheit(context, s);
-                    });
-                  })
-            ]),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: InkWell(
-              onTap: () {
-                Navigator.pop(context);
-                Navigator.pushNamed(context, WeatherRoutes.favoriteRoute);
-              },
-              child: Row(children: [
-                Text(
-                  WeatherAppString.favourite,
-                  style: WeatherAppFonts.medium(
-                      fontWeight: FontWeight.w400,
-                      color: WeatherAppColor.blackColor)
-                      .copyWith(fontSize: WeatherAppFontSize.s14),
-                ),
-                Expanded(child: Container()),
-                const Icon(Icons.star, color: Colors.amber,)
-              ]),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void setTheme(BuildContext context, bool? value) async {
-    theme =
-        value ?? await SharedprefsService.getData(WeatherAppString.isDarkTheme);
-    await SharedprefsService.isDarkTheme(theme);
-    final blocTheme = BlocProvider.of<SettingsThemeBloc>(context);
-    blocTheme.add(SetSettingsThemeEvent(theme));
-  }
-
-  void setFarenheit(BuildContext context,bool? value) async {
-    s = value ?? await SharedprefsService.getData(WeatherAppString.isFarenheit);
-    await SharedprefsService.isFarenHeit(s);
-    final blocFarenHeit = BlocProvider.of<SettingFarenheitBloc>(context);
-    blocFarenHeit.add(SetSettingsFarenheitEvent(s));
-  }
-
-  void _saveCity(WeatherModel weatherModel, BuildContext context) {
-    final userBloc = BlocProvider.of<SaveDataLocalBloc>(context);
-    userBloc.add(SaveLocalData(weatherModel));
-  }
-
-  void _getLocalData(BuildContext context) async{
-    final getLocalDataBloc = BlocProvider.of<GetLocalDataBloc>(context);
-    getLocalDataBloc.add(const GetLocalDataWeatherEvent());
-  }
-
-  void _getForeCastData(BuildContext context) {
-    final getForeCastLocalData = BlocProvider.of<GetForeCastBloc>(context);
-    getForeCastLocalData.add(const GetForeCastLocalDataEvent());
   }
 }
--- lib/presentation/screens/weather_home_screen/widgets/reusable_container.dart
+++ lib/presentation/screens/weather_home_screen/widgets/reusable_container.dart
@@ -1,19 +1,17 @@
 
 
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg_provider/flutter_svg_provider.dart';
 import 'package:weatherapps/const/app_color.dart';
 import 'package:weatherapps/const/app_extentions.dart';
 import 'package:weatherapps/const/appresources.dart';
-import 'package:weatherapps/presentation/controllers/weather_home_controller/weather_home.dart';
+import 'package:weatherapps/const/utils/utils.dart';
 import 'package:weatherapps/const/utils/weather_app_string.dart';
 import 'package:weatherapps/const/utils/weather_font_sizes.dart';
 import 'package:weatherapps/data/entity/forecast_entity.dart';
 
 import '../../../../const/utils/weather_app_fonts.dart';
-import '../../../controllers/settings_farenheit_controller/setting_farenheit_bloc.dart';
 
 class NextWeekCard extends StatelessWidget {
   final String daysOfWeek;
@@ -50,16 +48,19 @@ class NextWeekCard extends StatelessWidget {
           ),
       
           offline ? const Icon(Icons.cloud_off,size: 60.0,color: Colors.yellow) : Image.network(
-              WeatherHome.getWeatherIconURL(forecastModel.weather[0].icon!),
+              HomeUtils.getWeatherIconURL(forecastModel.weather[0].icon!),
               color: WeatherAppColor.yellowColor,
             ),
       
           /// temp
-          BlocBuilder<SettingFarenheitBloc,
-              SettingsFarenheitState>(
-              builder: (context, state) {
-                return setCelcius(state);
-              }),
+          Text(
+            "${(forecastModel.main!.temp! / 10).ceil()} ${WeatherAppString.celcius}",
+            style: WeatherAppFonts.large()
+                .copyWith(
+                    color: WeatherAppColor.whiteColor,
+                    fontWeight: FontWeight.w400)
+                .copyWith(fontSize: WeatherAppFontSize.s16),
+          ),
           SizedBox(
             height: 5.h,
           ),
@@ -95,38 +96,5 @@ class NextWeekCard extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Widget setCelcius(state) {
-    if (state is SetSettingFarenheit) {
-      if (state.farenheit) {
-        return Text(
-          "${(forecastModel.main!.temp! / 10).ceil()} ${WeatherAppString.farenheit}",
-          style: WeatherAppFonts.large()
-              .copyWith(
-              color: WeatherAppColor.whiteColor,
-              fontWeight: FontWeight.w400)
-              .copyWith(fontSize: WeatherAppFontSize.s16),
-        );
-      } else {
-        return Text(
-          "${(forecastModel.main!.temp! / 10).ceil()} ${WeatherAppString.celcius}",
-          style: WeatherAppFonts.large()
-              .copyWith(
-              color: WeatherAppColor.whiteColor,
-              fontWeight: FontWeight.w400)
-              .copyWith(fontSize: WeatherAppFontSize.s16),
-        );
-      }
-    } else {
-      return Text(
-        "${(forecastModel.main!.temp! / 10).ceil()} ${WeatherAppString.celcius}",
-        style: WeatherAppFonts.large()
-            .copyWith(
-            color: WeatherAppColor.whiteColor,
-            fontWeight: FontWeight.w400)
-            .copyWith(fontSize: WeatherAppFontSize.s16),
-      );
-    }
   }
 }
