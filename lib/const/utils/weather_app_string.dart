801a48c7bbce1b3d6eaab485370bfe06c1da4725
SirJackSparow <maulanabima785@gmail.com>
2024-05-14 18:17:10 -0700

    dev

--- lib/const/utils/weather_app_string.dart
+++ lib/const/utils/weather_app_string.dart
@@ -9,15 +9,8 @@ class WeatherAppString {
   static String kmh = "km/h";
   static String feelsLike = "Feels Like";
   static String search = 'Search';
-  static String cancel = 'Cancel';
   static String dataInsertFailed = 'Data Insert Failed';
   static String addFavorite = 'Add Favorite';
   static String listFavorite = 'List Favorite';
   static String drawerHeader = 'Menu';
-  static String darkTheme = 'Dark Theme';
-  static String farenheitName = 'FarenHeit';
-  static String isDarkTheme = 'isDarkTheme';
-  static String isFarenheit = 'isFarenheit';
-  static String favourite = "Favourite";
-
 }
--- lib/presentation/screens/favorite_screen/favorite_screen.dart
+++ lib/presentation/screens/favorite_screen/favorite_screen.dart
@@ -1,81 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weatherapps/const/utils/utils.dart';
 import 'package:weatherapps/presentation/controllers/favorite_controller/favorite_controller_bloc.dart';
-import 'package:weatherapps/presentation/controllers/settings_theme_controller/setting_theme_util.dart';
 
-import '../../../const/utils/weather_app_string.dart';
-import '../../../data/entity/weather_entity.dart';
-import '../../controllers/forecast_controller/forecast_controller_bloc.dart';
-import '../../controllers/settings_theme_controller/settings_theme_bloc.dart';
-import '../../controllers/weather_home_controller/weather_home_controller_bloc.dart';
-
-class FavoriteScreen extends StatelessWidget {
+class FavoriteScreen extends StatefulWidget {
   const FavoriteScreen({super.key});
 
   @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  @override
   Widget build(BuildContext context) {
-    _getFavorite(context);
-    return BlocBuilder<SettingsThemeBloc, SettingsThemeState>(
-      builder: (context,state) {
-        bool isDarkTheme = false;
-        if(state is SetSettingTheme){
-          isDarkTheme = state.themes;
+    HomeUtils.getFavorite(context);
+    return  Scaffold(
+      body: BlocBuilder<FavoriteBloc, FavoriteState>(builder: (context, states){
+        if(states is FavoriteDataLoaded) {
+          return ListView.builder(
+              itemCount: states.weatherModel.length,
+              itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(states.weatherModel[index].name),
+            );
+          }
+          );
+        } else if (states is FavoriteError){
+
         }
-        return Scaffold(
-          backgroundColor: isDarkTheme ? Colors.black : Colors.white,
-          appBar: AppBar(title: Text(WeatherAppString.favourite),),
-          body: BlocBuilder<FavoriteBloc, FavoriteState>(
-            builder: (context, state) {
-               if(state is FavoriteDataLoaded) {
-                 if(state.weatherModel.isEmpty) {
-                   return const Center(child: Text("Data is Empty"),);
-                 }
-                return ListView.builder(
-                    itemCount: state.weatherModel.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListTile(
-                        title: Text(state.weatherModel[index].name, style: TextStyle(
-                          color: isDarkTheme ? Colors.white : Colors.black
-                        ),),
-                        subtitle: Text(state.weatherModel[index].weather[0].description, style: TextStyle(
-                            color: isDarkTheme ? Colors.white : Colors.black
-                        ),),
-                        leading: IconButton(icon: const Icon(Icons.remove_circle_outline), color: Colors.red, onPressed: () {
-                          _deleteFavorite(state.weatherModel[index], context);
-                        },),
-                        onTap: () {
-                          _onClickCity(state.weatherModel[index].name,context);
-                          Navigator.pop(context);
-                        },
-                      );
-                    });
-              }
-             return const Center(child: Text("Error"),);
-            },
-          ),
-        );
-      }
+        return Container();
+      }),
     );
-  }
-
-  void _onClickCity(String cityName, BuildContext context) async {
-    final weatherCityBloc = BlocProvider.of<WeatherHomeControllerBloc>(context);
-    weatherCityBloc.add(GetCurrentCityWeatherInfo(cityName));
-    await _forecast(cityName, context);
-  }
-
-  _forecast(String cityName, BuildContext context) {
-    final foreCastBloc = BlocProvider.of<ForecastControllerBloc>(context);
-    foreCastBloc.add(GetForecast(cityName));
-  }
-
-  void _getFavorite(BuildContext context) async {
-    final favoriteBloc = BlocProvider.of<FavoriteBloc>(context);
-    favoriteBloc.add(const GetFavoriteEvent());
-  }
-  void _deleteFavorite(WeatherModel weatherModel, BuildContext context) {
-    final favoriteBloc = BlocProvider.of<FavoriteBloc>(context);
-    favoriteBloc.add(FavoriteSaveEvent(weatherModel));
   }
 }
