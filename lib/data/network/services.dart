be557829eab8e4024ca449e06cf0803c161295ee
SirJackSparow <maulanabima785@gmail.com>
2025-05-10 22:56:59 -0700

    delete apikey

--- lib/data/network/services.dart
+++ lib/data/network/services.dart
@@ -10,6 +10,5 @@ class WeatherAppServices {
   static const String iconURL = "https://openweathermap.org/img/wn/";
   static const String iconSize = "@2x.png";
 
-//https://api.openweathermap.org/data/2.5/weather?lat=-6.980423575803209&lon=110.41877708753705&appid=a97cfa41c1af3d8c87532b4ca93f6d14
-//api.openweathermap.org/data/2.5/forecast?q=mountain view&appid=a97cfa41c1af3d8c87532b4ca93f6d14
+
 }
